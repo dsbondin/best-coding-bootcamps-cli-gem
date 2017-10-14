007eeb524ed36fc2f98012f194ac10fed4c6cbe3
Denis Bondin <dsbondin@gmail.com>
2017-10-14 20:00:44 +0000

    changed some foldernames

---
+++ lib/best-coding-bootcamps-cli-gem.rb
@@ -0,0 +1,6 @@
+require "best-coding-bootcamps-cli-gem
+/version"
+
+module Bestcodingbootcampscligem
+  # Your code goes here...
+end
---
+++ lib/best-coding-bootcamps-cli-gem/version.rb
@@ -0,0 +1,3 @@
+module Bestcodingbootcampscligem
+  VERSION = "0.1.0"
+end
