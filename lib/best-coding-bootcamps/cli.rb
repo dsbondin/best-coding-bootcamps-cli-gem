816a9d4829dac2be984f81aa595f484eecd855f3
Denis Bondin <dsbondin@gmail.com>
2017-10-14 22:05:42 +0000

    working on cli.rb

--- lib/best-coding-bootcamps/cli.rb
+++ lib/best-coding-bootcamps/cli.rb
@@ -3,11 +3,25 @@ class BestCodingBootcamps::CLI
   def call
     puts "Best Coding Bootcamps in NYC"
     list_bootcamps
+    menu
   end
 
   def list_bootcamps
     puts "1. App Academy"
     puts "2. Flatiron School"
+  end
+
+  def menu
+    puts "Please enter the bootcamp number to get more information"
+    input = gets.strip.to_i
+    case input
+    when 1
+      puts "more info on 1"
+    when 2
+      puts "more info on 2"
+    end
+    end
+
   end
 
 end
