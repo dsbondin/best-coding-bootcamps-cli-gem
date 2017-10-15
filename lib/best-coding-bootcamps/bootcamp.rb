d0220e2aed775d7af3965e22159a66024f17e9ca
Denis Bondin <dsbondin@gmail.com>
2017-10-15 14:14:47 +0000

    defined structure in NOTES

--- lib/best-coding-bootcamps/bootcamp.rb
+++ lib/best-coding-bootcamps/bootcamp.rb
@@ -1,16 +1,7 @@
 class BestCodingBootcamps::Bootcamps
 
   attr_accessor :name, :about, :url, :courses, :cost_ranking, hiring_rate
-  @@all = []
 
-  def initialize(bootcamp_hash)
-
-    all << self
-  end
-
-  def self.all
-    @all
-  end
 
 
 
